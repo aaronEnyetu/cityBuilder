b0ae43c75aecad27a67dc21c70afdddc20be006b
Aaron Enyetu <aaronenyetu@gmail.com>
2022-04-28 17:28:03 -0700

    Grabbed DOM elements and set event listener for the second dropdown

--- app.js
+++ app.js
@@ -2,12 +2,19 @@
 const ecocityDropdown = document. getElementById('ecoCity-dropdown');
 const ecocityEl = document.getElementById('eco');
 
+const futurecityDropdown = document. getElementById('futureCity-dropdown');
+const futurecityEl = document.getElementById('future');
 // let state
 
 // set event listeners 
 
 ecocityDropdown.addEventListener('change', () => {
     ecocityEl.style.backgroundImage = `url('./assets/${ecocityDropdown.value}-eco.jpg')`;
+});
+
+futurecityDropdown.addEventListener('change', () => {
+    futurecityEl.style.backgroundImage = `url('./assets/${futurecityDropdown.value}-future.jpg')`;
+
 });
   // get user input
   // use user input to update state 
